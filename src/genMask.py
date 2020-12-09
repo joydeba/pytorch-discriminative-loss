70453a575b6b36c727a6f710bfeda96af25b1128
Ubuntu <ubuntu@joysdeepimg.arbutus>
2020-12-09 02:10:26 +0000

    Data

--- src/genMask.py
+++ src/genMask.py
@@ -1,45 +1,78 @@
 import os
 import cv2
 import json
-import numpy as np 
+import numpy as np
+import math  
 
-source_folder = os.path.join(os.getcwd(), "images")
-json_path = "maskGen_json.json"                     # Relative to root directory
+source_folder = os.path.join(os.getcwd(), "ethz_1/images")
+# json_path = "maskGen_json.json"                     # Relative to root directory
 count = 0                                           # Count of total images saved
 file_bbs = {}                                       # Dictionary containing polygon coordinates for mask
-MASK_WIDTH = 256									# Dimensions should match those of ground truth image
-MASK_HEIGHT = 256									
+MASK_WIDTH = 1024									# Dimensions should match those of ground truth image
+MASK_HEIGHT = 1024									
 
 # Read JSON file
-with open(json_path) as f:
-  data = json.load(f)
+# with open(json_path) as f:
+#   data = json.load(f)
+
+file_bbs = {}
+
+from xml.dom import minidom
+
+xmlpath = 'ethz_1/rotated_xml'
+for filename in os.listdir(xmlpath):
+    if not filename.endswith('.xml'): continue
+    fullname = os.path.join(xmlpath, filename)
+    fname = os.path.splitext(filename)[0]
+    xmldoc = minidom.parse(fullname)
+    itemlist = xmldoc.getElementsByTagName('robndbox')
+    print(len(itemlist))
+    all_points = []
+    for s in itemlist:
+        x1 = float(s.getElementsByTagName('cx')[0].firstChild.nodeValue)
+        y1= float(s.getElementsByTagName('cy')[0].firstChild.nodeValue)
+        x2 = x1 + float(s.getElementsByTagName('w')[0].firstChild.nodeValue) 
+        y2 = y1 + float(s.getElementsByTagName('h')[0].firstChild.nodeValue)
+        theta = float(s.getElementsByTagName('angle')[0].firstChild.nodeValue)
+
+        xmin = x1
+        ymin = y1
+        xmax = x1+(x1-x1)*math.cos(theta)+(y2-y1)*math.sin(theta)
+        ymax = y1-(x2-x1)*math.sin(theta)+(y2-y1)*math.cos(theta)
+
+        all_points.append([xmin,ymin])
+        all_points.append([xmin,ymax])
+        all_points.append([xmax,ymax])
+        all_points.append([xmax,ymin])
+    file_bbs[fname] = all_points
+
 
 # Extract X and Y coordinates if available and update dictionary
-def add_to_dict(data, itr, key, count):
-    try:
-        x_points = data[itr]["regions"][count]["shape_attributes"]["all_points_x"]
-        y_points = data[itr]["regions"][count]["shape_attributes"]["all_points_y"]
-    except:
-        print("No BB. Skipping", key)
-        return
+# def add_to_dict(data, itr, key, count):
+#     try:
+#         x_points = data[itr]["regions"][count]["shape_attributes"]["all_points_x"]
+#         y_points = data[itr]["regions"][count]["shape_attributes"]["all_points_y"]
+#     except:
+#         print("No BB. Skipping", key)
+#         return
     
-    all_points = []
-    for i, x in enumerate(x_points):
-        all_points.append([x, y_points[i]])
+#     all_points = []
+#     for i, x in enumerate(x_points):
+#         all_points.append([x, y_points[i]])
     
-    file_bbs[key] = all_points
+#     file_bbs[key] = all_points
   
-for itr in data:
-    file_name_json = data[itr]["filename"]
-    sub_count = 0               # Contains count of masks for a single ground truth image
+# for itr in data:
+#     file_name_json = data[itr]["filename"]
+#     sub_count = 0               # Contains count of masks for a single ground truth image
     
-    if len(data[itr]["regions"]) > 1:
-        for _ in range(len(data[itr]["regions"])):
-            key = file_name_json[:-4] + "*" + str(sub_count+1)
-            add_to_dict(data, itr, key, sub_count)
-            sub_count += 1
-    else:
-        add_to_dict(data, itr, file_name_json[:-4], 0)
+#     if len(data[itr]["regions"]) > 1:
+#         for _ in range(len(data[itr]["regions"])):
+#             key = file_name_json[:-4] + "*" + str(sub_count+1)
+#             add_to_dict(data, itr, key, sub_count)
+#             sub_count += 1
+#     else:
+#         add_to_dict(data, itr, file_name_json[:-4], 0)
 
 			
 print("\nDict size: ", len(file_bbs))
